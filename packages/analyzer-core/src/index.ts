7e0759bf9cc340267fe2932379ffd7950eefad64
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2026-09-19 09:49:26 +0700

    feat: establish deterministic analyzer core [NFR-001] [NFR-004]

---
+++ packages/analyzer-core/src/index.ts
@@ -0,0 +1,38 @@
+export { runAnalyzer } from "./analyzer.js";
+export type { AnalyzerDefinition, AnalyzerRunInput } from "./analyzer.js";
+
+export type {
+  AnalysisContext,
+  FactRuleContext,
+  FindingRuleContext,
+  RecommendationRuleContext
+} from "./context.js";
+
+export {
+  AnalyzerConfigurationError,
+  AnalyzerInvariantError
+} from "./errors.js";
+
+export { runRulePipeline } from "./pipeline.js";
+export type { RulePipelineResult } from "./pipeline.js";
+
+export { assembleAnalysisReport } from "./report.js";
+export type { AnalysisReportAssemblyInput } from "./report.js";
+
+export {
+  validateFactRuleResult,
+  validateFindingRuleResult,
+  validateRecommendationRuleResult
+} from "./rules.js";
+export type {
+  AnalysisRuleSet,
+  FactRule,
+  FactRuleResult,
+  FindingRule,
+  FindingRuleResult,
+  RecommendationRule,
+  RecommendationRuleResult,
+  RuleDefinition
+} from "./rules.js";
+
+export type { AnalysisScorer, ScoringContext } from "./scoring.js";
