2e4f892914a0069a67eb015a958c76328cb66bba
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2026-09-19 09:56:12 +0700

    refactor: keep analyzer errors minimal [NFR-004]

--- packages/analyzer-core/src/errors.ts
+++ packages/analyzer-core/src/errors.ts
@@ -1,15 +1,7 @@
 export class AnalyzerConfigurationError extends Error {
   override readonly name = "AnalyzerConfigurationError";
-
-  constructor(message: string) {
-    super(message);
-  }
 }
 
 export class AnalyzerInvariantError extends Error {
   override readonly name = "AnalyzerInvariantError";
-
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
-  }
 }
